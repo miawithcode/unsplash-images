e72f680f402f5601114709df1d9027dd8e4f408b
miawithcode <cuttontail@gmail.com>
2024-02-03 17:48:57 +0800

    feat: setup react query

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,6 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+const url = 'https://api.unsplash.com/search/photos?client_id=ACESS_KEY&query=office';
+
 const Gallery = () => {
-  return (
-    <div>Gallery</div>
-  )
-}
-export default Gallery
+  const response = useQuery({
+    queryKey: ['images'],
+    queryFn: async () => {
+      const result = await axios.get(url);
+      return result.data
+    },
+  });
+
+  console.log(response);
+
+  return <div>Gallery</div>;
+};
+export default Gallery;
